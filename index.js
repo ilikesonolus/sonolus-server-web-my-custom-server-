0f02e985d2ac3ee797d00574954d50c67436d314
ilikesonolus <ivp1722@gmail.com>
2025-11-05 17:18:32 +0200

    Serve index.html on root GET request

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const __dirname = path.dirname(__filename)
 
 const app = express()
 
+// если index.html лежит в корне
 app.use(express.static(__dirname))
+
+// чтобы точно отдать index.html при GET /
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
